f1aeef184e5dbb334b2f6aa64032f01fd2acbd97
alyshamaewood <70618051+alyshamaewood@users.noreply.github.com>
2021-02-02 20:01:14 -0500

    first commit

---
+++ fortune_teller.py
@@ -0,0 +1,25 @@
+title = input("Shall I tell your fortune? y/n ")
+
+user_choice_colors = input("Choose a color: yellow, pink, red, or green: ")
+
+# user_choice_yellow_numbers = ["2", "3", "6", "7"]
+
+# user_choice_yellow2 = ["2", "3", "6", "7"]
+
+
+if (title == "y"):
+    input("Choose a color: yellow, pink, red, or green: ")
+    if (user_choice_colors == "yellow"):
+        input("Choose a number: 2, 3, 6, 7: ")
+    elif ()
+# if (user_choice_yellow_numbers == "2"):
+#     input("Choose a number: 2, 3, 6, 7: ")
+# if (user_choice_yellow2 == "2"):
+#     print("You have been granted a health spell. ")
+
+
+else:
+    print("Awh, we can play another day. :<")
+
+
+
